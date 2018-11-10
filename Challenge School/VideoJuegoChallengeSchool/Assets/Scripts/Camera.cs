f3ad60083dcfd9b1bc8373507a633e7009505fe7
Stephany Diaz <asocoba96@hotmail.com>
2018-11-10 12:42:56 -0500

    1

---
+++ Challenge School/VideoJuegoChallengeSchool/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform character;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void LateUpdate()
+    {
+        this.transform.localPosition = new Vector3(character.transform.localPosition.x, 4, -20);
+    }
+}
---
+++ Challenge School/VideoJuegoChallengeSchool/Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour {
+
+    public Animator animatorCharacter;
+    bool isRunning = false;
+	// Use this for initialization
+	void Start () {
+        animatorCharacter = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isRunning = false;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * 0.3f);
+            isRunning = true;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * 0.3f);
+            isRunning = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        animatorCharacter.SetBool("RunningPersonajePrincipal", isRunning);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 8.0f, ForceMode2D.Impulse);
+            animatorCharacter.SetBool("FloorPersonajePrincipal", false);
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D col){
+        animatorCharacter.SetBool("FloorPersonajePrincipal", true);
+    }
+}
